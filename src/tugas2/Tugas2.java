7a1596c5e2c8663ad15d0f5ef2e29ee5db33aa40
Trisno Marsono <trisnomarsono@yahoo.com>
2017-06-16 19:02:38 +0700

    Tugas2 Java3visual

---
+++ src/tugas2/Tugas2.java
@@ -0,0 +1,25 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas2;
+
+import frmTugas.frmTugas2;
+
+/**
+ *
+ * @author acer
+ */
+public class Tugas2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmTugas2 out = new frmTugas2();
+        out.setVisible(true);
+    }
+    
+}
